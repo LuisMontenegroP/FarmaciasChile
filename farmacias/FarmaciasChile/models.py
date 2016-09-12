9e4a1e39f38ec1757580c9c1c2bbdfd7a6238e33
Carlo Echeverría <carlo.echeverria@alumnos.upla.cl>
2016-09-11 22:42:05 -0300

    @LuisMontenegroP @mguevara
    GeoFarmCL con la base de farmacias importada al proyecto.

--- farmacias/FarmaciasChile/models.py
+++ farmacias/FarmaciasChile/models.py
@@ -1,5 +1,22 @@
 from __future__ import unicode_literals
 
 from django.db import models
+	
+class Farmacias(models.Model):
+    local_id = models.IntegerField(primary_key=True)
+    local_nombre = models.TextField(blank=True, null=True)
+    comuna_nombre = models.TextField(blank=True, null=True)
+    localidad_nombre = models.TextField(blank=True, null=True)
+    local_direccion = models.TextField(blank=True, null=True)
+    funcionamiento_hora_apertura = models.TextField(blank=True, null=True)
+    funcionamiento_hora_cierre = models.TextField(blank=True, null=True)
+    local_telefono = models.TextField(blank=True, null=True)
+    local_lat = models.TextField(blank=True, null=True)
+    local_lng = models.TextField(blank=True, null=True)
 
-# Create your models here.
+    def __unicode__(self):
+		return self.local_nombre
+
+    class Meta:
+        managed = False
+        db_table = 'farmacias'
